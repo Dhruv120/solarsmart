3b9daa3d4aa1c038e55047bde0a714eb08297baf
Mr Dhruv Dholariya <64961371+Dhruv120@users.noreply.github.com>
2022-06-24 09:25:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,97 @@
+AOS.init({
+	duration: 1000,
+    //once: true,
+    disable: 'mobile'
+});
+
+// Menu JS
 $(document).ready(function(){
-    // $sections incleudes all of the container divs that relate to menu items.
-    var $sections = $('.container');
-    
-    // The user scrolls
-    $(window).scroll(function(){
-      
-      // currentScroll is the number of pixels the window has been scrolled
-      var currentScroll = $(this).scrollTop();
-      
-      // $currentSection is somewhere to place the section we must be looking at
-      var $currentSection
-      
-      // We check the position of each of the divs compared to the windows scroll positon
-      $sections.each(function(){
-        // divPosition is the position down the page in px of the current section we are testing      
-        var divPosition = $(this).offset().top;
-        
-        // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
-        // the -1 is so that it includes the div 1px before the div leave the top of the window.
-        if( divPosition - 1 < currentScroll ){
-          // We have either read the section or are currently reading the section so we'll call it our current section
-          $currentSection = $(this);
-          
-          // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
-        }
-        
-        // This is the bit of code that uses the currentSection as its source of ID
-        var id = $currentSection.attr('id');
-          $('a').removeClass('active');
-          $("[href=#"+id+"]").addClass('active');
-        
-      })
-  
-    });
+  $(".mobile__menu").click(function(){
+    $(this).toggleClass("show");
+    $(".navbar").toggleClass("show");
   });
+});
+// Menu JS
+
+// Header Fixed JS
+jQuery(document).ready(function()
+{
+    jQuery(window).scroll(function() {    
+        var scroll = jQuery(window).scrollTop();
+    
+        if (scroll >= 100) {
+            jQuery("header").addClass("fixed-header");
+        } else {
+            jQuery("header").removeClass("fixed-header");
+        }
+    });
+});
+// Header Fixed JS
+
+//for section move on click
+$(document).on('click', 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top - 100
+  }, 800);
+});
+
+// Page Redirect To Section JS
+
+// Get all sections that have an ID defined
+
+const sections = document.querySelectorAll("section[id]");
+// Add an event listener listening for scroll
+
+window.addEventListener("scroll", navHighlighter);
+function navHighlighter() {
+
+  // Get current scroll position
+  let scrollY = window.pageYOffset; 
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop;
+    sectionId = current.getAttribute("id");   
+
+    /*
+
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+
+    */
+
+    if (
+      scrollY > sectionTop - 100 &&
+      scrollY <= sectionTop + sectionHeight - 100
+    ){
+      document.querySelector(".menu__bar a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".menu__bar a[href*=" + sectionId + "]").classList.remove("active");
+    }
+    if ($(window).width() < 1600) {
+        if (
+          scrollY > sectionTop - 80 &&
+          scrollY <= sectionTop + sectionHeight - 80
+        ){
+          document.querySelector(".menu__bar a[href*=" + sectionId + "]").classList.add("active");
+        } else {
+          document.querySelector(".menu__bar a[href*=" + sectionId + "]").classList.remove("active");
+        }
+    }
+    if ($(window).width() < 1025) {
+        if (
+          scrollY > sectionTop - 90 &&
+          scrollY <= sectionTop + sectionHeight - 90
+        ){
+          document.querySelector(".menu__bar a[href*=" + sectionId + "]").classList.add("active");
+        } else {
+          document.querySelector(".menu__bar a[href*=" + sectionId + "]").classList.remove("active");
+        }
+    }
+
+  });
+}
+// Page Redirect To Section JS
